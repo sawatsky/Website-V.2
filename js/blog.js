81e125b2522bda90e662409140e3e4ae5323ac22
sawatsky <heather.sawatsky@icloud.com>
2016-08-18 16:28:40 -0700

    Adding basic functionality for getting blog files based on what link user clicks, including 'random' selection

--- js/blog.js
+++ js/blog.js
@@ -1,14 +1,21 @@
 (function(document, undefined) {
-	var customLink = document.querySelector("site-subnavbar-link[custom-link]");
-
-	customLink.style.right = 0 - customLink.offsetParent.offsetLeft + 35 + "px";
-
-	var xmlHttpRequest = new XMLHttpRequest();
-	xmlHttpRequest.onreadystatechange = function() {
-		if (xmlHttpRequest.readyState === 4 && (xmlHttpRequest.status === 200 || xmlHttpRequest.status == 0)) {
-			console.log(JSON.parse(xmlHttpRequest.responseText));
+	var links = document.querySelectorAll("site-subnavbar-link");
+	links.forEach(function(link) {
+		if (link.hasAttribute("custom-link")) {
+			link.style.right = 0 - link.offsetParent.offsetLeft + 35 + "px";
 		}
-	}
-	xmlHttpRequest.open("GET", "blogs/jquery.json", true);
-	xmlHttpRequest.send();
+
+		link.onclick = function() {
+			var xmlHttpRequest = new XMLHttpRequest();
+			xmlHttpRequest.onreadystatechange = function() {
+				if (xmlHttpRequest.readyState === 4 && (xmlHttpRequest.status === 200 || xmlHttpRequest.status == 0)) {
+					console.log(JSON.parse(xmlHttpRequest.responseText));
+					// if user clicked random link, randomly select one of the blogs from the response text to display
+					// Otherwise, display list of all blogs from response text
+				}
+			}
+			xmlHttpRequest.open("GET", "blogs/" + (link.hasAttribute("src") ? link.getAttribute("src") : links[Math.floor(Math.random() * links.length)]).getAttribute("src"), true);
+			xmlHttpRequest.send();
+		}
+	});
 })(document)

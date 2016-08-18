446fec7ff2ce1cde1a9a0e33b49ad27a85f6e18a
sawatsky <heather.sawatsky@icloud.com>
2016-08-18 16:13:43 -0700

    Beginning to add Blog content

--- js/blog.js
+++ js/blog.js
@@ -2,4 +2,13 @@
 	var customLink = document.querySelector("site-subnavbar-link[custom-link]");
 
 	customLink.style.right = 0 - customLink.offsetParent.offsetLeft + 35 + "px";
+
+	var xmlHttpRequest = new XMLHttpRequest();
+	xmlHttpRequest.onreadystatechange = function() {
+		if (xmlHttpRequest.readyState === 4 && (xmlHttpRequest.status === 200 || xmlHttpRequest.status == 0)) {
+			console.log(JSON.parse(xmlHttpRequest.responseText));
+		}
+	}
+	xmlHttpRequest.open("GET", "blogs/jquery.json", true);
+	xmlHttpRequest.send();
 })(document)
